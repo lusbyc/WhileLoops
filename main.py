77a0662bdd7374df538d79ff59600f375633b85e
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-05 01:07:49 -0500

    changed camel case to snake case

--- main.py
+++ main.py
@@ -2,23 +2,23 @@
 
 #1
 number = 1
-userInput = int(input("Please enter a whole number greater than or equal to 1: "))
-print(f"Thank you. Numbers {number} through {userInput} are displayed below.")
-while number <= userInput:
+user_input = int(input("Please enter a whole number greater than or equal to 1: "))
+print(f"Thank you. Numbers {number} through {user_input} are displayed below.")
+while number <= user_input:
     print(number)    
-    if number == userInput:
+    if number == user_input:
         break
     number += 1
 
 #2
 number = 1
-userInput = int(input("Please enter a whole number greater than 1: "))
-print(f"Thank you. Numbers {userInput} through {number} are displayed below.")
-while userInput >= number:
-    print(userInput)
-    if userInput == number:
+user_input = int(input("Please enter a whole number greater than 1: "))
+print(f"Thank you. Numbers {user_input} through {number} are displayed below.")
+while user_input >= number:
+    print(user_input)
+    if user_input == number:
         break
-    userInput -= 1
+    user_input -= 1
 
 #3 
 print()
@@ -41,7 +41,7 @@ print()
 
 #4
 number = 0
-print("Even numbers between 1 and 100 are displayed below.")
+print("Even numbers from 1 and 100 are displayed below.")
 while number < 100:
     number += 1
     while number % 2 == 0:
@@ -52,7 +52,7 @@ while number < 100:
  
 #5 
 number = 0
-print("Odd numbers between 1 and 100 are displayed below.")
+print("Odd numbers from 1 and 100 are displayed below.")
 while number < 100:
     number += 1
     while number % 2 != 0:
@@ -64,23 +64,23 @@ while number < 100:
 #6
 number = 1
 sum = 0
-userInput= int(input("Please enter a number greater than 1: "))
-print(f"The sum of all whole numbers from {number} to {userInput} are displayed below.")
-while number <= userInput:
+user_input= int(input("Please enter a number greater than 1: "))
+print(f"The sum of all whole numbers from {number} to {user_input} are displayed below.")
+while number <= user_input:
     sum = sum + number
     number += 1
-    if number > userInput:
+    if number > user_input:
         print(sum)    
         break
 
  
 #7
 print('Multiplication Table')
-userInput = int(input("Please enter a whole number: "))
+user_input = int(input("Please enter a whole number: "))
 number = 1
-while number <= userInput:
-    product = userInput * number
-    print(f"{userInput} * {number} = {product}")
+while number <= user_input:
+    product = user_input * number
+    print(f"{user_input} * {number} = {product}")
     number += 1
-    if number > userInput:
+    if number > user_input:
         break
